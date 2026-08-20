0d8c4b35fd7413ba373f88becdf5ae66baf99129
trishamadhusudhan <trisha30339@gmail.com>
2026-08-20 21:21:41 +0530

    adding project detail link

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import './ProjectCard.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './ProjectCard.css';
+
 function ProjectCard({ project }) {
   return (
-      <article className="project-card">
-        <div className="project-card-body">
-            <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <div className="project-tags">
-                {/* We map over the array of tech to create span tags */}
-                {project.tech.map((t, index) => (
-                  <span key={index} className='tag'>{t}</span>
-                ))}
-              </div>
-              <button className='btn btn-primary'>View Project</button>
+    <article className="project-card">
+      <div className="project-card-body">
+        <h3>{project.title}</h3>
+
+        <p>{project.description}</p>
+
+        <div className="project-tags">
+          {project.tech.map((t, index) => (
+            <span key={index} className="tag">
+              {t}
+            </span>
+          ))}
         </div>
-      </article>
-  )
+
+        <Link
+          to={`/projects/${project.id}`}
+          className="btn btn-primary"
+        >
+          View Project
+        </Link>
+      </div>
+    </article>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
