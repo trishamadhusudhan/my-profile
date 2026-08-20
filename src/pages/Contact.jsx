cc025ba12b1d02215d772657a28ae992167dab0f
trishamadhusudhan <trisha30339@gmail.com>
2026-08-20 21:27:49 +0530

    update contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,13 +3,40 @@ import ContactForm from '../components/ContactForm/ContactForm';
 
 function Contact() {
   return (
-    <div className='contact-page container'>
-      <section id='contact' className='contact-section' style={{ padding: '40px 0' }}>
+    <div className="contact-page container">
+      <section
+        id="contact"
+        className="contact-section"
+        style={{ padding: '40px 0' }}
+      >
         <h1>Contact Me</h1>
-        <p>If you'd like to work together, feel free to reach out using the form below.</p>
+
+        <p>If you'd like to work together, feel free to reach out.</p>
+
+        <p>
+          Email:{' '}
+          <a href="mailto:trishamadhusudhan04@gmail.com">
+            trishamadhusudhan04@gmail.com
+          </a>
+        </p>
+
+        <p>
+          LinkedIn:{' '}
+          <a
+            href="https://www.linkedin.com/in/trisha-madhusudhan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Trisha M
+          </a>
+        </p>
+
         <ContactForm />
+
         <div style={{ marginTop: '30px' }}>
-          <Link to='/' className='btn btn-secondary'>Back to Home</Link>
+          <Link to="/" className="btn btn-secondary">
+            Back to Home
+          </Link>
         </div>
       </section>
     </div>
@@ -17,4 +44,3 @@ function Contact() {
 }
 
 export default Contact;
-
