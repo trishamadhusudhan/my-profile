093bd0a95b14dba95aabd4b5ef3fab5076f860ab
trishamadhusudhan <trisha30339@gmail.com>
2026-08-20 21:26:51 +0530

    footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <footer className='site-footer'>
             <p>
-        &copy;{year} Rakesh K R. Built with React🌐
+        &copy;{year} Trisha M. Built with React🌐
     </p>
         </footer>
     )
